47c42729dfdcdac75e82bb2a76ec5eb5bc2b83a7
arshakebrahim <151016679+arshakebrahim@users.noreply.github.com>
2024-07-21 14:30:39 +0530

    first commit

---
+++ lib/pages/homescreen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Column(
+        children: [
+          ListView(
+            children: [
+             ListTile(
+            leading: Checkbox(value: value, onChanged: onChanged),
+            text
+             )
+            ],
+          )
+        ],
+      )),
+    );
+  }
+}
